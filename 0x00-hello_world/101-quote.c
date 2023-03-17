33cfef27a21d1bc153931412ea38253041cbec3f
Mohamed <namez.mohamed@gmail.com>
2023-03-17 13:24:21 -0700

    Modified 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) 
+/**
+  *main - Entery point
+  *
+  *Return: Always 1 (success)
+  */
+int main(void)
 {
     fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
     return (0);
