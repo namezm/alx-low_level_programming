8cf2afc2ecf5b150da85e0dfc0cf69df71e2ad6a
Mohamed <namez.mohamed@gmail.com>
2023-03-17 15:15:18 -0700

    Modified 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char x = 0;
+	char y = 'a';
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar('0' + x);
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar(y);
+	}  
+		putchar('\n');
+
+	return (0);
+}
