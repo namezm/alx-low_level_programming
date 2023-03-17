7c237ff3baf485a4757878e91eeb821035b76245
Mohamed <namez.mohamed@gmail.com>
2023-03-17 13:38:35 -0700

    Modified 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
- */
+  *main - Entery point
+  *
+  *Return: Always 1 (success)
+  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
