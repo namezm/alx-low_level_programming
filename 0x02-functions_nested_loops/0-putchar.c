4de2401336a78f40ed45f5afbeb800f3db3f68f4
Mohamed <namez.mohamed@gmail.com>
2023-03-21 12:57:56 -0700

    Modified 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible different combinations
+ * of two two-digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
