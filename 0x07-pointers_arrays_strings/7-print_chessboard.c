37620f3cd93f1cb02072d0b44a448f2f2576f6c3
Mohamed <namez.mohamed@gmail.com>
2023-04-04 10:37:15 -0700

    Modified 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_chessboard - chessborad with C
+ *@a: pointer
+ *Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			_putchar(' ');
+		}
+		_putchar('\n')
+	}
+}
