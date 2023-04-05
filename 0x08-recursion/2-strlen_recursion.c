b22a2b049b06fd7818509a2e60c0cda537dcc7f7
Mohamed <namez.mohamed@gmail.com>
2023-04-05 11:06:28 -0700

    Modified 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_strlen_recursion - lenght of string
+ *@s: string
+ *Return: nothing
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + !));
+}
