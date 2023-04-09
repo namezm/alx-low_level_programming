e75fccf8d532f06fb6a3c61f2f16b3e642f87119
Mohamed <namez.mohamed@gmail.com>
2023-04-09 12:41:05 -0700

    Modified 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - print number of arg
+ *@argc: count
+ *@argv: value
+ *Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
