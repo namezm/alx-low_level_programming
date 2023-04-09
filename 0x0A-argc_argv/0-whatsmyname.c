8ae2724ed0a037da06e09814aa5b0b3daec6f68d
Mohamed <namez.mohamed@gmail.com>
2023-04-09 12:32:38 -0700

    Modified 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- *main - printmyname
+ *main - print programme name
  *@argc: count
  *@argv: value
  *Return: nothing
  */
 int main(int argc, char *argv[])
 {
-	printf('%s', argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
