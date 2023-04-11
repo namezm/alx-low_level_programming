234d4e70c5b56aa6f7802c1bcdac51af9c73185e
Mohamed <namez.mohamed@gmail.com>
2023-04-11 15:41:26 -0700

    Modified 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,18 +9,20 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	if (!s1) s1 = "";
-	if (!s2) s2 = "";
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
 
 	unsigned int len1 = strlen(s1);
 	unsigned int len2 = strlen(s2);
 	char *result;
 
 	result = (char *) malloc(len1 + len2 + 1;
-	if (!result) return (NULL);
-	
+	if (!result)
+		return (NULL);
 	memcpy(result, s1, len1);
 	memcpy(result + len1, s2, len2 + 1);
 
-	return result;	
+	return (result);
 }
