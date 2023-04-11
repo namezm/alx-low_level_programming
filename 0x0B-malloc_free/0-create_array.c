ecd6709a037fd19d5817161b5da657be0ec0eac7
Mohamed <namez.mohamed@gmail.com>
2023-04-11 15:08:20 -0700

    Modified 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,9 @@ char *create_array(unsigned int size, char c)
 	if (array == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
+	unsigned int i;
+	
+	for (i = 0; i < size; i++)
 	{
 		*array[i] = c;
 	}
