184d13297af75170a9ebbab985e260440a4fcbf3
Mohamed <namez.mohamed@gmail.com>
2023-04-17 11:06:10 -0700

    Modified dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,13 @@ struct dog
 };
 
 
+/**
+ *dog_t - new name for the type struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 
 #endif
