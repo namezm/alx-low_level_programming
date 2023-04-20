beb49ce8341c11d54e4641b5a92b00367ea64a7e
Mohamed <namez.mohamed@gmail.com>
2023-04-20 15:48:21 -0700

    Modified 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ *sum_them_all - the sum of all its parameters
+ *@n: parameters
+ *Return: total
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
