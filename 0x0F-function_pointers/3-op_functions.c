6526a1fff634baf1a8f83e5157b141b9149cf6bb
Mohamed <namez.mohamed@gmail.com>
2023-04-20 15:27:23 -0700

    Modified 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -18,7 +18,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-        return (a - b);
+	return (a - b);
 }
 
 /**
@@ -29,7 +29,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 
 /**
@@ -45,7 +45,7 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-        return (a / b);
+	return (a / b);
 }
 
 /**
@@ -61,6 +61,6 @@ int op_mod(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-        return (a % b);
+	return (a % b);
 }
 
