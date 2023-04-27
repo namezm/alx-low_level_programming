0f0c8ce2b27329d8426cc1391613eabfdb1ac940
namezm <namez.mohamed@gmail.com>
2023-04-27 10:53:32 -0700

    Modified 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - verification
+ * @h: listheader
+ * Return: list size
+ */
+size_t print_list(const list_t *h)
+{
+	size_t cp = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u] %s\n", h->str == NULL ? 0 : h->len
+		       , h->str == NULL ? "(nil)" : h->str);
+		cp++;
+		h = h->next;
+	}
+	return (cp);
+}
