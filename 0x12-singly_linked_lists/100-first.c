9356623210647bf57eda66455a31fcca6269b0a0
namezm <namez.mohamed@gmail.com>
2023-04-27 11:08:52 -0700

    Modified 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void pre_main_function() {
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+int main() {
+    pre_main_function();
+
+
+    return 0;
+}
