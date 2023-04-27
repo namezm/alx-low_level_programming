7c5c0c16978b0389b0d67affef8f5e9c1219f965
namezm <namez.mohamed@gmail.com>
2023-04-27 10:53:19 -0700

    Modified lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+void _putchar(char);
+/**
+ * struct list_s - Singly linked lists
+ * @str: string
+ * @len: cp 
+ * @next: points to node
+ *
+ * Description: Singly linked lists
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *);
+size_t list_len(const list_t *);
+list_t *add_node(list_t **, const char *);
+list_t *add_node_end(list_t **, const char *);
+void free_list(list_t *);
+
+#endif
