daba6975e9cf167a9d27a74a19d0eb58f2b07bb8
namezm <namez.mohamed@gmail.com>
2023-05-01 05:06:36 -0700

    Modified 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,17 +3,17 @@
 /**
  * pop_listint - deletes the head node
  * @h: liste
- * Return: contente of a head liste
+ * Return: contente of a liste
  */
 int pop_listint(listint_t **h)
 {
-	int cp = 0;
 	listint_t *o_head = *h;
+	int z = 0;
 
 	if (o_head == NULL)
-		return (cp);
-	cp = o_head->cp;
+		return (z);
+	z = o_head->z;
 	*h = o_head->next;
 	free(o_head);
-	return (cp);
+	return (z);
 }
