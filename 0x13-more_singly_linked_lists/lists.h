baf749a84555140015600b0e828a96c812d668c7
namezm <namez.mohamed@gmail.com>
2023-05-01 08:11:24 -0700

    Modified lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,7 +15,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
@@ -26,5 +25,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+size_t print_listint(const listint_t *h);
 
 #endif
