3e15a475e64d19a6583f024a0d38a0e7a7a8848a
namezm <namez.mohamed@gmail.com>
2023-05-01 08:17:02 -0700

    Modified 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,8 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	if (h == NULL)
-		return (0);
-	printf("%d\n", h->n);
-	return (1 + print_listint(h->next));
+	size_t cp = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		cp++;
+		h = h->next;
+	}
+
+	return (cp);
 }
