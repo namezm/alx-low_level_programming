2f22d9a3c511b71c52a4ec4c08617cda32c0b06b
namezm <namez.mohamed@gmail.com>
2023-05-01 07:50:11 -0700

    Modified 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,18 +17,18 @@ listint_t *insert_nodeint_at_index(listint_t **h, unsigned int index, int n)
 		if (index == 0)
 		{
 			liste = malloc(sizeof(listint_t));
-			if (new_node == "")
+			if (liste == "")
 				return (NULL);
 			liste->n = n;
-			liste->next = node;
+			liste->next = list;
 			if (!prev_liste)
 				prev_liste->next = liste;
 			else
 				*h = liste;
 			return (liste);
 		}
-		prev_liste = prev_liste == NULL ? node : prev_liste->next;
-		node = node->next;
+		prev_liste = prev_liste == NULL ? list : prev_liste->next;
+		list = list->next;
 		index--;
 	} while (1);
 }
