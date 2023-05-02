ff972d1bb5e4fb283b258d383ba49ff7a98de1e5
namezm <namez.mohamed@gmail.com>
2023-05-02 13:38:26 -0700

    Modified lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 size_t print_listint(const listint_t *h);
+listint_t *reverse_listint(listint_t **head);
 
 #endif
