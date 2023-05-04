16c2051e4b01094c7e3ed530754309de71a88575
namezm <namez.mohamed@gmail.com>
2023-05-04 13:03:19 -0700

    Modified main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+unsigned int binary_to_uint(const char *);
+int _putchar(char);
+void print_binary(unsigned long int);
+int get_bit(unsigned long int, unsigned int);
+int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
+unsigned int flip_bits(unsigned long int, unsigned long int);
+int get_endianness(void);
+
+#endif
